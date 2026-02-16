3892d0cf8b82b72cb224538010d016d2a3ab1430
NicolasGaffe <nicolas.gaffe@efrei.net>
2026-02-16 23:56:57 +0100

    Add files via upload

---
+++ inscription.php
@@ -0,0 +1,12 @@
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=Impact-Frame;charset=utf8", "nicolas", "1234");
+
+$pseudo = $_POST['pseudo'];
+$email  = $_POST['email'];
+$mdp    = $_POST['mot_de_passe'];
+
+$ins = $pdo->prepare("INSERT INTO Utilisateurs (pseudo, email, mot_de_passe) VALUES (?, ?, ?)");
+$ins->execute([$pseudo, $email, $mdp]);
+
+header('Location: login.html');
+?>
