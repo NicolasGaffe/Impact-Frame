205a83496bf8439e3b637f8ae26731afde4be242
NicolasGaffe <nicolas.gaffe@efrei.net>
2026-02-16 23:33:05 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=Impact-Frame;charset=utf8", "nicolas", "1234");
+?>
---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require 'config.php';
+
+$pseudo = $_POST['pseudo'];
+$email  = $_POST['email'];
+$mdp    = $_POST['mot_de_passe'];
+
+$req = $pdo->prepare("SELECT * FROM Utilisateurs WHERE pseudo = ? AND email = ? AND mot_de_passe = ?");
+$req->execute([$pseudo, $email, $mdp]);
+$user = $req->fetch();
+
+if ($user) {
+    $_SESSION['pseudo'] = $user['pseudo'];
+    $_SESSION['email']  = $user['email'];
+    header('Location: index.html');
+} else {
+    header('Location: login.html');
+}
