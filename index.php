eaaf81fe2ef8470a0a55196d1fd39b0214b138f8
NicolasGaffe <nicolas.gaffe@efrei.net>
2026-02-17 20:20:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,39 +10,39 @@ session_start();
     <title>Impact Frame</title>
 </head>
 <nav class="navbar">
-    <a href="Tableau.html">Nos Tableaux</a>
-    <ul>
-        <li><a href="Tableau_Manga.html">Par Manga</a></li>
-        <li><a href="Tableau_Nouveau.html">Quoi de Nouvau ?</a></li>
-    </ul>
-    <a href="Figurine.html">Nos Figurines</a>
-    <ul>
-        <li><a href="Figurine_Manga.html">Par Manga</a></li>
-        <li><a href="Figurine_Nouveau.html">Quoi de Nouvau ?</a></li>
-    </ul>
-    <a href="Katana.html">Nos Katanas</a>
-    <ul>
-        <li><a href="Katana_Manga.html">Par Manga</a></li>
-        <li><a href="Katana_Nouveau.html">Quoi de Nouvau ?</a></li>
-    </ul>
-    <a href="Entreprise.html">L'entreprise</a>
-    <ul>
-        <li><a href="Magasin.html">Le Magasin</a></li>
-        <li><a href="ServiceClient.html">Service Client</a></li>
-        <li><a href="FAQ.html">FAQ</a></li>
-        <li><a href="Politique.html">Politique de Confidentialité</a></li>
-    </ul>
-    <a href="Cadeau.html">Carte Cadeau</a>
-    <?php if (isset($_SESSION['pseudo'])): ?>
-        <a href="MonCompte.php">Mon Compte (<?php echo htmlspecialchars($_SESSION['pseudo']); ?>)</a>
-        <a href="logout.php">Déconnexion</a>
-    <?php else: ?>
-        <a href="Login.html">Se Connecter</a>
-        <a href="Register.html">S'inscrire</a>
-    <?php endif; ?>
-    <a href="Panier.html">Panier</a>
+    <div class="nav-links">
+        <div class="dropdown">
+            <a href="Tableau.html" class="main-link">Tableaux</a>
+            <div class="dropdown-content">
+                <a href="Tableau_Manga.html">Par Manga</a>
+                <a href="Tableau_Nouveau.html">Nouveautés</a>
+            </div>
+        </div>
+
+        <div class="dropdown">
+            <a href="Figurine.html" class="main-link">Figurines</a>
+            <div class="dropdown-content">
+                <a href="Figurine_Manga.html">Par Manga</a>
+                <a href="Figurine_Nouveau.html">Nouveautés</a>
+            </div>
+        </div>
+
+        <a href="Katana.html" class="main-link">Katanas</a>
+        <a href="Entreprise.html" class="main-link">L'entreprise</a>
+    </div>
+
+    <div class="nav-auth">
+        <?php if (isset($_SESSION['pseudo'])): ?>
+            <a href="MonCompte.php">Compte (<?php echo htmlspecialchars($_SESSION['pseudo']); ?>)</a>
+            <a href="logout.php" style="color: #ff4d4d;">Déconnexion</a>
+        <?php else: ?>
+            <a href="Login.html">Connexion</a>
+        <?php endif; ?>
+        <a href="Panier.html" class="cart-btn">🛒 Panier</a>
+    </div>
 </nav>
 </html>
+
 
 
 
