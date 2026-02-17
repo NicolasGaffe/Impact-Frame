79331cd2bd83e84e826e2ed20f49adfa5d4bca81
NicolasGaffe <nicolas.gaffe@efrei.net>
2026-02-17 20:28:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,16 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="style.css">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Impact Frame</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body> <nav class="navbar">
+<body> 
+
+<nav class="navbar">
     <div class="nav-links">
         <div class="dropdown">
             <a href="Tableau.html" class="main-link">Tableaux</a>
@@ -44,6 +47,7 @@ session_start();
 
 </body>
 </html>
+
 
 
 
