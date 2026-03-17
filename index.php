f00359018c9188120e69b3b6d7991c829f64b88e
NicolasGaffe <nicolas.gaffe@efrei.net>
2026-03-17 13:43:44 +0100

    Fix HTML encoding issues in index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ session_start();
             </div>
             
             <div class="nav-item">
-                <a href="Entreprise.html">L'entreprise</a>
+                <a href="Entreprise.php">L'entreprise</a>
                 <ul class="dropdown">
                     <li><a href="Magasin.html">Notre Magasin</a></li>
                     <li><a href="Service_Client.html">Service Client</a></li>
