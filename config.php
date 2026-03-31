57d450506d853f763ff71eff95d710b0af225a8a
NicolasGaffe <nicolas.gaffe@efrei.net>
2026-03-31 15:16:07 +0200

    Fix database name case in config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=Impact-Frame;charset=utf8", "admin", "1234");
+$pdo = new PDO("mysql:host=localhost;dbname=impact_frame;charset=utf8", "admin", "1234");
 ?>
