3313dbad812223d51a84eb7c787f024d5c457928
NicolasGaffe <nicolas.gaffe@efrei.net>
2026-03-31 15:15:18 +0200

    Fix HTML encoding issues in index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ session_start();
                 <a href="logout.php" class="logout">Se déconnecter</a>
             <?php else: ?>
                 <a href="Login.html">Connexion</a>
-                <a href="inscription.php">S'inscrire</a>
+                <a href="Register.html">S'inscrire</a>
             <?php endif; ?>
             <a href="Panier.html" class="cart">🛒 Panier</a>
         </div>
